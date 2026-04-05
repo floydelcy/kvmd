8e0aaefe07c74da483b77806092e9eaf9261303e
Maxim Devaev <mdevaev@gmail.com>
2026-04-05 22:04:06 +0300

    removed some noisy logging

--- kvmd/apps/otg/__init__.py
+++ kvmd/apps/otg/__init__.py
@@ -404,7 +404,7 @@ def _cmd_stop(config: Section) -> None:
         _unlink(join(config.otg.meta, meta))
     _rmdir(config.otg.meta)
 
-    logger.info("Bye-bye")
+    logger.info("Successfully stopped")
 
 
 # =====
